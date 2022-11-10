6b8ab5236fa5927a85b49367b18de84ba438d2ff
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-10 11:45:17 +0530

    Add files via upload

---
+++ test_connection_GCP.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 11:39:48 2022
+
+@author: AbishekH
+"""
+
+import streamlit as sl
+# import requests
+# from bs4 import BeautifulSoup
+
+url = "https://storage.cloud.google.com/img-2-insulin-bucket-1/Palak_Paneer_1.jpg"
+sl.image(url)
