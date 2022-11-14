643c4029e47fc0dccd2acba2eb6791c04a85e055
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-14 15:28:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ if st.session_state.page ==1:
         try:
             databaseConfig = yaml.safe_load(file)
             item_names = databaseConfig.get('names')
-            st.write(item_names.keys)
+            st.write(list(item_names.keys()))
             for code in item_codes_from_text:
                 if code in item_names.keys():
                     st.write(item_names.get(str(code)))
