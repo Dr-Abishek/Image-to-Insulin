737d913a7d2790c4a5519af837ad5ff14d180f28
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-15 10:27:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,6 @@ st.markdown('---')
 st.markdown("### Items Detected: ")
 st.markdown("Please click the checkbox to confirm")
 
-write_to_text = open("food_qty_text.txt",'w')
 with open('custom_data.yaml') as file:
     try:
         databaseConfig = yaml.safe_load(file)
@@ -58,10 +57,7 @@ with open('custom_data.yaml') as file:
             option = form2.checkbox(label=food_item,value=False)
             if option:
                 qty = form2.text_input("No. of servings of "+food_item,max_chars=3)
-                if qty:
-                    form2.write(food_item,qty)
-                    write_to_text.write(food_item+"\t"+str(qty)+"\n")
-                
+                st.write(food_item,qty)
     except yaml.YAMLError as exc:
         st.write(exc)
 f2_sb = form2.form_submit_button("Submit")
@@ -72,17 +68,14 @@ st.write(f"check = {check}")
 
 ######### Page 3
 
-f = open("food_qty_text.txt",'r')
-food_qty_text = f.read()
-st.write(food_qty_text)
-sugar_level_offset=0
+#sugar_level_offset=0
 
-blood_sugar_prior_meal = st.text_input("Enter your blood sugar prior to the meal",max_chars=3)
-st.write("Assuming a normal blood sugar level of 120...")
-if blood_sugar_prior_meal != '':
-    sugar_level_offset=int(blood_sugar_prior_meal)-120
+b#lood_sugar_prior_meal = st.text_input("Enter your blood sugar prior to the meal",max_chars=3)
+#st.write("Assuming a normal blood sugar level of 120...")
+#if blood_sugar_prior_meal != '':
+#    sugar_level_offset=int(blood_sugar_prior_meal)-120
 
-total_carbs_in_meal = 0
+#total_carbs_in_meal = 0
 
 #for food_item,qty in final_df:
 #    total_carbs_in_meal += int(qty)*carb_calc(food_item)
@@ -91,8 +84,3 @@ total_carbs_in_meal = 0
 
 #if recommended_insulin:
     #st.markdown("## Your recommended Insulin Dosage as per the [Healthline website](https://www.healthline.com/health/how-much-insulin-to-take-chart#how-to-calculate) is "+str(recommended_insulin)+" units")
-
-        
-        
-upload()
-detect_food_and_qty()
