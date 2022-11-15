26f6475cb5ca3b61656e9c3ce6accad4bd25dbe5
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-15 15:28:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,20 +58,20 @@ elif st.session_state.count == 2:
         #final_list = Read_Yaml(item_codes_from_text)
         final_list = []
         with open('custom_data.yaml') as file:
-        try:
-            qty=0
-            databaseConfig = yaml.safe_load(file)
-            item_names = databaseConfig.get('names')
-            for item_code in item_codes_from_text:
-                food_item = item_names[int(item_code)]
-                option = st.checkbox(label=food_item,value=False)
-                if option:
-                    qty = st.text_input("No. of servings of "+food_item,max_chars=3)
-                    if qty:
-                        final_list.append([food_item,qty])
+            try:
+                qty=0
+                databaseConfig = yaml.safe_load(file)
+                item_names = databaseConfig.get('names')
+                for item_code in item_codes_from_text:
+                    food_item = item_names[int(item_code)]
+                    option = st.checkbox(label=food_item,value=False)
+                    if option:
+                        qty = st.text_input("No. of servings of "+food_item,max_chars=3)
+                        if qty:
+                            final_list.append([food_item,qty])
 
-        except yaml.YAMLError as exc:
-            st.write(exc)
+            except yaml.YAMLError as exc:
+                st.write(exc)
         f0.close()
 
         st.write("final_list")
