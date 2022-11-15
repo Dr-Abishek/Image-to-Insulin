b3cd5380b8ae4e55f1f7671063f501aecbb8d98b
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-15 10:52:55 +0530

    Create get_item_codes.py

---
+++ get_item_codes.py
@@ -0,0 +1,12 @@
+#Read the text file and obtain the item codes
+def item_codes(txt_path):
+  f = open(txt_path+".txt", "r")
+  text_result = f.read()
+  item_codes_from_text = []
+  list_from_text = text_result.split()
+  for i in range(len(list_from_text)):
+      code = int(float(list_from_text[i]))
+      if float(list_from_text[i]) == code:
+          if list_from_text[i] not in item_codes_from_text:
+              item_codes_from_text.append(list_from_text[i])
+  return(item_codes_from_text)
