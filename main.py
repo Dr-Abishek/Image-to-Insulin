c9387c71e65f6bd7078e892b91d86eadcca3e50a
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-15 11:47:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,63 +10,66 @@ from read_yaml import Read_Yaml
 
 
 st.title("Image-to-Insulin calculator")
-check = 0
+if "page" not in st.session_state:
+    st.session_state.page = 0
+
+def nextpage(): st.session_state.page += 1
+def restart(): st.session_state.page = 0
+
+placeholder = st.empty()
+st.button("Next",on_click=nextpage,disabled=(st.session_state.page > 3))
+
 final_list = []
 
 ##### PAGE 1
-form1 = st.form("Upload")
-form1.subheader("Upload your meal image to scan for food items")
+if st.session_state.page == 0:
+    form1 = placeholder.form("Upload")
+    form1.subheader("Upload your meal image to scan for food items")
 
-image=form1.file_uploader("Please upload an image", type=['png','jpg','jpeg'], accept_multiple_files=False)
-if image is not None:
-    with open(os.path.join("yolov5/","temp_image.jpg"),"wb") as f: 
-      f.write(image.getbuffer())         
-
-f1_sb = form1.form_submit_button("Submit")
-if f1_sb:
-    st.write("Successfully submitted")
-    check+=1
-st.write(f"check = {check}")
+    image=form1.file_uploader("Please upload an image", type=['png','jpg','jpeg'], accept_multiple_files=False)
+    if image is not None:
+        with open(os.path.join("yolov5/","temp_image.jpg"),"wb") as f: 
+          f.write(image.getbuffer())         
     
 ######### Page 2
-#Inference
 
-txt_path = run(weights='last.pt', data = 'custom_data.yaml', source="yolov5/"+"temp_image.jpg") # Returns the path to the text file containing the results of the inference
+elif st.session_state.page == 1:
+    #Inference
 
-item_codes_from_text = item_codes(txt_path)
+    txt_path = run(weights='last.pt', data = 'custom_data.yaml', source="yolov5/"+"temp_image.jpg") # Returns the path to the text file containing the results of the inference
 
-#Infer the items according to item codes from the yaml file
-st.markdown('---')
-st.markdown("### Items Detected: ")
-st.markdown("Please click the checkbox to confirm")
+    item_codes_from_text = item_codes(txt_path)
 
-final_list = Read_Yaml(item_codes_from_text)
-        
-f2_sb = st.button("Submit")
-if f2_sb:
-    st.write("Successfully submitted")
-    check+=1
-    st.write(final_list)
-st.write(f"check = {check}")
+    #Infer the items according to item codes from the yaml file
+    placeholder.markdown('---')
+    placeholder.markdown("### Items Detected: ")
+    placeholder.markdown("Please click the checkbox to confirm")
 
+    final_list = Read_Yaml(item_codes_from_text)
+
+
+    #
+   
 ######### Page 3
-st.markdown('---')
-sugar_level_offset=0
+elif st.session_state.page == 2:
+    st.write(final_list)
+    st.markdown('---')
+    sugar_level_offset=0
 
-blood_sugar_prior_meal = st.text_input("Enter your blood sugar prior to the meal",max_chars=3)
+    blood_sugar_prior_meal = st.text_input("Enter your blood sugar prior to the meal",max_chars=3)
 
-if blood_sugar_prior_meal != '':
-    st.write("Assuming a normal blood sugar level of 120...")
-    sugar_level_offset=float(blood_sugar_prior_meal)-120
+    if blood_sugar_prior_meal != '':
+        st.write("Assuming a normal blood sugar level of 120...")
+        sugar_level_offset=float(blood_sugar_prior_meal)-120
 
-total_carbs_in_meal = 0
+    total_carbs_in_meal = 0
 
-for row in final_list:
-    food = row[0]
-    qty = row[1]
-    total_carbs_in_meal += int(qty)*carb_calc(food_item=food)
-st.write("Total carbs in your meal, as calculated by scraping [Swasthi's Recipes] (https://www.indianhealthyrecipes.com/) is "+str(total_carbs_in_meal) + "g")
-recommended_insulin = round(( (sugar_level_offset/50) + (total_carbs_in_meal) /10) *2.0)/2.0
+    for row in final_list:
+        food = row[0]
+        qty = row[1]
+        total_carbs_in_meal += int(qty)*carb_calc(food_item=food)
+    st.write("Total carbs in your meal, as calculated by scraping [Swasthi's Recipes] (https://www.indianhealthyrecipes.com/) is "+str(total_carbs_in_meal) + "g")
+    recommended_insulin = round(( (sugar_level_offset/50) + (total_carbs_in_meal) /10) *2.0)/2.0
 
-if recommended_insulin:
-    st.markdown("## Your recommended Insulin Dosage as per the [Healthline website](https://www.healthline.com/health/how-much-insulin-to-take-chart#how-to-calculate) is "+str(recommended_insulin)+" units")
+    if recommended_insulin:
+        st.markdown("## Your recommended Insulin Dosage as per the [Healthline website](https://www.healthline.com/health/how-much-insulin-to-take-chart#how-to-calculate) is "+str(recommended_insulin)+" units")
