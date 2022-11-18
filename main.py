d8ff9823d24c2f7ec554aa457228075d3375e157
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-18 14:05:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,11 +38,12 @@ elif st.session_state.count == 1:
     #Inference
     with placeholder.container():
         st.write("Detecting food items..." )
-        dataset,txt_path = run(weights='last.pt', data = 'custom_data.yaml', source="yolov5/"+"temp_image.jpg") # Returns the path to the text file containing the results of the inference
+        o,i,txt_path = run(weights='last.pt', data = 'custom_data.yaml', source="yolov5/"+"temp_image.jpg") # Returns the path to the text file containing the results of the inference
         item_codes_from_text = item_codes(txt_path)
         st.write("Click 'Next' to see detected items")
-        st.write(str(type(dataset))+str(len(dataset)))
-
+        st.write(o)
+        st.write(i)
+        
         f = open("temp.txt", "w")
         for item_codes in item_codes_from_text:
             f.write(str(item_codes)+"\t")
