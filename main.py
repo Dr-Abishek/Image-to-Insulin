e20b9a3ae3a99d0ac5c4d7ec0c196d99ad03f6da
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-26 22:17:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 import streamlit as st
-from yolov5.detect import run
-import os
-import yaml
-import lxml
-import numpy as np
-from support_files.ingredient_scraper import carb_calc
-from support_files.get_item_codes import item_codes
-from support_files.read_yaml import Read_Yaml
 import i2i_calc
-#import pandas as pd
 
 # Title of the main page
 st.title("Image-to-Insulin Calculator")
 def nextpage(): st.session_state.count += 1
 def restart(): st.session_state.count = 1
 
+user_id = None
+
 def login():
   st.header("Login")
-  user_id = int(st.number_input("Enter user id:"))
+  user_id = int(st.number_input("Enter user id:",step=1))
   submit = st.button("Submit")
   if user_id is not None:
     try:
@@ -30,10 +23,17 @@ def signup():
   st.header("Signup")
   
 def calc():
-  final_list_2, total_carbs_in_meal, recommended_insulin = i2i_calc.app()
+  if user_id is not None:
+    final_list_2, total_carbs_in_meal, recommended_insulin = i2i_calc.app()
+  else:
+    st.write("Please log in with your user id first")
             
 def dashboard():
-  st.header("Dashboard")
+  if user_id is not None:
+    st.header("Dashboard")
+  else:
+    st.write("Please log in with your user id first")
+  
 
 page = st.sidebar.selectbox('Select page',['Login','Signup','Calculate','Dashboard'])
 if page == 'Login':
