581b858916f6a6e4b5a84dbd657f757964c0cf9c
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-26 19:31:21 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,3 +1,12 @@
 import streamlit as st
 def login_app():
-  st.title("Login/Signup")
+  st.title("Login")
+  user_id = int(st.number_input("Enter user id:"))
+  submit = st.button("Submit")
+  if user_id is not None:
+    try:
+      if submit is not None:
+        return user_id
+    except:
+      st.write("User id not found. Please sign up")
+  
