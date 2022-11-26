2a1e509d7815ef6c3e8a7734bc3c82da932cb85f
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-27 01:38:35 +0530

    Update read_table.py

--- psql/read_table.py
+++ psql/read_table.py
@@ -2,10 +2,10 @@ import psycopg2
 from psql.config import config
 import pandas as pd
 
-df = pd.DataFrame([],columns=["date", "food", "carbs", "insulin"])
+
 
 def get_info(user_id):
-    
+    df = pd.DataFrame([],columns=["date", "food", "carbs", "insulin"])
     sql = f"""
             SELECT date, food, carbs, insulin 
             FROM info_table 
@@ -22,7 +22,7 @@ def get_info(user_id):
         row = cur.fetchone()
 
         while row is not None:
-            df.append(list(row))
+            df.loc[len(df)] = list(row)
             row = cur.fetchone()
 
         cur.close()
@@ -32,6 +32,32 @@ def get_info(user_id):
     finally:
         if conn is not None:
             conn.close()
-        return df    
-if __name__ == '__main__':
-    get_info(user_id=1)
+        return df
+    
+def get_all_info():
+    
+    sql = f"""
+            SELECT user_id, date, food,carbs, insulin 
+            FROM info_table
+            ORDER BY date
+           """
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute(sql)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+        
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+        return None
