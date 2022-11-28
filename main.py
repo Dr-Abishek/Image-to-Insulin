083e650582134ef23954070ad142c068b840b818
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-28 11:46:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from psql.create_table import create_tables
 # Title of the main page
 st.title("Image-to-Insulin Calculator")
 create_tables()
+full_user_df = get_all_users()
 user_id = None
 
 def login():
@@ -19,7 +20,7 @@ def login():
   submit = st.button("Submit")
   if user_id is not None:
     try:
-      if submit is not False:
+      if submit and (user_id in full_user_df['user_id']):
         f = open("user.txt", "w")
         f.write(str(user_id))
         f.close()
@@ -27,13 +28,17 @@ def login():
       st.write("User id not found. Please sign up")
 
 def signup():
+  
   form = st.form("Signup")
+  st.table(full_user_df['email'])
   name = form.text_input("Enter your name:")
   email = form.text_input("Enter your email:")
   submit_state = form.form_submit_button("submit")
   if submit_state:
     if name== "" or email=="":
         st.warning("Please fill all fields")
+    elif email in full_user_df['email']:
+        st.warning("Email id already exists. Please enter another email")
     else:
         generated_id = insert_user(name,email)
         st.success(f"Successfully submitted. Your user id is {generated_id}")
@@ -84,7 +89,6 @@ elif page == 'Dashboard':
     dashboard()
 elif page == 'Full Data':
     full_info_df = get_all_info()
-    full_user_df = get_all_users()
     if full_info_df is not None:
       st.table(full_info_df)
     if full_user_df is not None:
