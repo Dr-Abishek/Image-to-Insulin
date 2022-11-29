80776d202422c1a52d9901e70e7f253091db75d7
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-29 14:42:27 +0530

    Update get_model_and_labels.py

--- support_files/get_model_and_labels.py
+++ support_files/get_model_and_labels.py
@@ -2,7 +2,7 @@ from azure.storage.blob import BlobClient
 import streamlit as st
 
 def get_blob(blob_name):
-  blob = BlobClient(account_url="https://abishekimage2insulin.blob.core.windows.net"
+  blob = BlobClient(account_url="https://abishekimage2insulin.blob.core.windows.net",
                     container_name="food-image-dataset",
                     blob_name=blob_name,
                     credential="7aZbmTQutOM7HrpUi7Fjxb6jhQpHQsD+8PTLtRoTYk6zLmn6iA7T7feOHxLwulkfYwFTY1hxLfl++ASt09srdw==")
