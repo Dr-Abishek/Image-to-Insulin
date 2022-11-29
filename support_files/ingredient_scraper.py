ed0e1ecc9b6e4666f85a970f4346bdbe278ff8f6
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-29 09:27:57 +0530

    Update ingredient_scraper.py

--- support_files/ingredient_scraper.py
+++ support_files/ingredient_scraper.py
@@ -13,7 +13,7 @@ Outputs of this program:
 import requests
 import streamlit as st
 from bs4 import BeautifulSoup
-from psql.carb_info_db import create_tables, carb_info_db, update_carb_info_db
+from psql.carb_info_db import carb_info_db, update_carb_info_db
 
 def carb_calc(
         main_url = "https://www.indianhealthyrecipes.com/",
@@ -26,7 +26,7 @@ def carb_calc(
     try:
         food_id, carb_content_in_grams = carb_info_db(food_item)
     except:
-        create_tables()
+        
         page=requests.get(f"{main_url}{food_item}")
         soup=BeautifulSoup(page.content, features="lxml")
         rows=soup.findAll("div",class_="nutrition-item nutrition-item-carbohydrates")
