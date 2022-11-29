3301e9897668b76f56a599e620db16054a436979
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-29 15:02:44 +0530

    Update get_model_and_labels.py

--- support_files/get_model_and_labels.py
+++ support_files/get_model_and_labels.py
@@ -1,9 +1,10 @@
-from azure.storage.blob import BlobClient
+from azure.storage.blob import BlobClient, BlobService
 import streamlit as st
 
-def get_blob(blob_name):
-  blob = BlobClient(account_url="https://abishekimage2insulin.blob.core.windows.net",
-                    container_name="food-image-dataset",
-                    blob_name=blob_name,
-                    credential="7aZbmTQutOM7HrpUi7Fjxb6jhQpHQsD+8PTLtRoTYk6zLmn6iA7T7feOHxLwulkfYwFTY1hxLfl++ASt09srdw==")
-  return(blob)
+def get_blob():
+  #blob = BlobClient(account_url="https://abishekimage2insulin.blob.core.windows.net",
+                   #container_name="food-image-dataset",
+                  # blob_name=blob_name,
+                    #credential="7aZbmTQutOM7HrpUi7Fjxb6jhQpHQsD+8PTLtRoTYk6zLmn6iA7T7feOHxLwulkfYwFTY1hxLfl++ASt09srdw==")
+  blob_service = BlobService(account_name='abishekimage2insulin', account_key='7aZbmTQutOM7HrpUi7Fjxb6jhQpHQsD+8PTLtRoTYk6zLmn6iA7T7feOHxLwulkfYwFTY1hxLfl++ASt09srdw==')
+  return(blob_service)
