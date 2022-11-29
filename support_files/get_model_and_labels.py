ad34915a60166fd035d5615162668c554ac87ab5
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-29 14:40:16 +0530

    Create get_model_and_labels.py

---
+++ support_files/get_model_and_labels.py
@@ -0,0 +1,9 @@
+from azure.storage.blob import BlobClient
+import streamlit as st
+
+def get_blob(blob_name):
+  blob = BlobClient(account_url="https://abishekimage2insulin.blob.core.windows.net"
+                    container_name="food-image-dataset",
+                    blob_name=blob_name,
+                    credential="7aZbmTQutOM7HrpUi7Fjxb6jhQpHQsD+8PTLtRoTYk6zLmn6iA7T7feOHxLwulkfYwFTY1hxLfl++ASt09srdw==")
+  return(blob)
