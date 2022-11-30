5db43c3c49ee7531137971f9d8f022f4c610fb8f
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-30 10:15:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -88,14 +88,30 @@ def dashboard():
       #columns: date, food carbs, insulin
       opt=st.sidebar.radio("Choose time frame for viewing stats.", options=("day",'week','month','year'))
       if opt == 'day':
-        st.success(type(today))
-        st.success(type(df['date'][0]))
+        df_day = df[df['date'] = today]
+        df_day['carbs'].hist()
+        df_day['insulin'].unique().hist()
+        st.markdown("---")
+        st.markdown(f"#### Total carbs consumed for today: {sum(df_day['carbs']})
+        #st.markdown(f"#### Total insulin dosage for today: {sum(df_day['insulin']})
       elif opt =='week':
-        pass
+        df_week = df[df['date'].isocalendar().week = today.isocalendar().week)
+        df_week['carbs'].hist(bins = min(len(df_week,7))
+        df_week['insulin'].unique().hist(min(len(df_week,7))
+        st.markdown("---")
+        st.markdown(f"#### Total carbs consumed for today: {sum(df_week['carbs']})
       elif opt == 'month':
-        pass
+        df_month = df[df['date'].month = today.month]
+        df_month['carbs'].hist(bins = min(len(df_month,30))
+        df_month['insulin'].unique().hist(bins = min(len(df_month,30))
+        st.markdown("---")
+        st.markdown(f"#### Total carbs consumed for today: {sum(df_month['carbs']})                                                        
       elif opt =='year':
-        pass
+        df_year = df[df['date'].year = today.year]
+        df_year['carbs'].hist(bins = min(len(df_year,12))
+        df_year['insulin'].unique().hist(bins = min(len(df_year,30))
+        st.markdown("---")
+        st.markdown(f"#### Total carbs consumed for today: {sum(df_year['carbs']})                                                        
       logout = st.button("Logout")
       if logout:
         f0 = open("user.txt", "w"); f0.write(""); f0.close();
