a954199aa1393101f9cab31ff408255f60e5f42b
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-11-30 12:56:53 +0530

    Update plot_graphs.py

--- support_files/plot_graphs.py
+++ support_files/plot_graphs.py
@@ -3,18 +3,25 @@ import streamlit as st
 
 def plot_graphs(df):
   col1,col2 = st.columns(2)
+  df = df.set_index('date')
   
   fig1 = plt.figure()
+  df['carbs'].plot(kind='hbar')
+  plt.title("Carbs")
+  
+  
   fig2 = plt.figure()
-  ax1 = fig1.add_subplot(111)
-  ax1.set_title("Carbs")
-  ax2 = fig2.add_subplot(111)
-  ax2.set_title("Insulin")
+  df['insulin'].plot(kind='hbar')
+  plt.title("Insulin")
+  #ax1 = fig1.add_subplot(111)
+  #ax1.set_title("Carbs")
+  #ax2 = fig2.add_subplot(111)
+  #ax2.set_title("Insulin")
   
-  #df = df.set_index('date')
+  #
   
-  ax1.barh(df['date'],df['carbs'])
-  ax2.barh(df['date'],df['insulin'])
+  #ax1.barh(df['date'],df['carbs'])
+  #ax2.barh(df['date'],df['insulin'],color = 'red')
   
   col1.write(fig1)
   col2.write(fig2)
