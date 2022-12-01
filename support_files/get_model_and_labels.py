68363bc0148d28940c7e1c080eb6bd2d76eb332e
Dr-Abishek <117620010+Dr-Abishek@users.noreply.github.com>
2022-12-01 21:11:32 +0530

    Update get_model_and_labels.py

--- support_files/get_model_and_labels.py
+++ support_files/get_model_and_labels.py
@@ -35,7 +35,7 @@ def upload_blob_to_azure(blob,type_of_blob,user_id):
     if type_of_blob == "img":
       blob_name = "temp_img_"+str(user_id)+".jpg"
     else:
-      blob_name = "temp_txt_"+str(user_id)+".jpg"
+      blob_name = "temp_txt_"+str(user_id)+".txt"
     blob_client = blob_service_client.get_blob_client(container=container_name, blob = blob_name)
     #with open(file=blob, mode="rb") as data:
     blob_client.upload_blob(blob)
